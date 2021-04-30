73bbbf606fb160d3c89e8e5e611287f58920b05d
James McLeod <james.mcleod@finos.org>
2021-04-30 13:00:38 +0100

    add features config

---
+++ website/src/components/feature-config.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+export const features = [
+    {
+        title: <>Open Source Project Collaboration</>,
+        imageUrl: '/img/intro/Collaboration.svg',
+        description: (
+            <>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam a nunc quis ligula venenatis maximus ac hendrerit urna. In diam enim, facilisis vitae mattis et, mattis non magna. Nullam condimentum in dui nec molestie. Nunc faucibus, erat sit amet tincidunt egestas, est enim rutrum massa, non sagittis est tortor a diam. Duis rhoncus interdum dolor eget posuere. Vestibulum aliquam vitae augue commodo pretium. Curabitur vel eros velit. Cras nisl ex, commodo eget massa vel, iaculis sodales nunc.
+            </>
+        ),
+    },
+    {
+        title: <>Financial Services Working Together</>,
+        imageUrl: '/img/intro/Bank.svg',
+        description: (
+            <>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam a nunc quis ligula venenatis maximus ac hendrerit urna. In diam enim, facilisis vitae mattis et, mattis non magna. Nullam condimentum in dui nec molestie. Nunc faucibus, erat sit amet tincidunt egestas, est enim rutrum massa, non sagittis est tortor a diam. Duis rhoncus interdum dolor eget posuere. Vestibulum aliquam vitae augue commodo pretium. Curabitur vel eros velit. Cras nisl ex, commodo eget massa vel, iaculis sodales nunc.
+            </>
+        ),
+    },
+    {
+        title: <>Technology Vendors Working Together</>,
+        imageUrl: '/img/intro/TechCommunication.svg',
+        description: (
+            <>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam a nunc quis ligula venenatis maximus ac hendrerit urna. In diam enim, facilisis vitae mattis et, mattis non magna. Nullam condimentum in dui nec molestie. Nunc faucibus, erat sit amet tincidunt egestas, est enim rutrum massa, non sagittis est tortor a diam. Duis rhoncus interdum dolor eget posuere. Vestibulum aliquam vitae augue commodo pretium. Curabitur vel eros velit. Cras nisl ex, commodo eget massa vel, iaculis sodales nunc.
+            </>
+        )
+    },
+    {
+        title: <>Accelerating Your Open Source Delivery Pipeline</>,
+        imageUrl: '/img/intro/Success.svg',
+        description: (
+            <>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam a nunc quis ligula venenatis maximus ac hendrerit urna. In diam enim, facilisis vitae mattis et, mattis non magna. Nullam condimentum in dui nec molestie. Nunc faucibus, erat sit amet tincidunt egestas, est enim rutrum massa, non sagittis est tortor a diam. Duis rhoncus interdum dolor eget posuere. Vestibulum aliquam vitae augue commodo pretium. Curabitur vel eros velit. Cras nisl ex, commodo eget massa vel, iaculis sodales nunc.
+            </>
+        )
+    }
+];
--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -10,6 +10,8 @@ import Project from '../components/project';
 import Feature from '../components/feature';
 import Vendor from '../components/vendor';
 
+import { features } from '../components/feature-config';
+
 const projects = [
   {
     "name": "FDC3",
@@ -42,45 +44,6 @@ const vendors = [
     name: 'Red Hat'
   }
 ]
-
-const features = [
-  {
-    title: <>Open Source Project Collaboration</>,
-    imageUrl: '/img/intro/Collaboration.svg',
-    description: (
-      <>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam a nunc quis ligula venenatis maximus ac hendrerit urna. In diam enim, facilisis vitae mattis et, mattis non magna. Nullam condimentum in dui nec molestie. Nunc faucibus, erat sit amet tincidunt egestas, est enim rutrum massa, non sagittis est tortor a diam. Duis rhoncus interdum dolor eget posuere. Vestibulum aliquam vitae augue commodo pretium. Curabitur vel eros velit. Cras nisl ex, commodo eget massa vel, iaculis sodales nunc.
-      </>
-    ),
-  },
-  {
-    title: <>Financial Services Working Together</>,
-    imageUrl: '/img/intro/Bank.svg',
-    description: (
-      <>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam a nunc quis ligula venenatis maximus ac hendrerit urna. In diam enim, facilisis vitae mattis et, mattis non magna. Nullam condimentum in dui nec molestie. Nunc faucibus, erat sit amet tincidunt egestas, est enim rutrum massa, non sagittis est tortor a diam. Duis rhoncus interdum dolor eget posuere. Vestibulum aliquam vitae augue commodo pretium. Curabitur vel eros velit. Cras nisl ex, commodo eget massa vel, iaculis sodales nunc.
-      </>
-    ),
-  },
-  {
-    title: <>Technology Vendors Working Together</>,
-    imageUrl: '/img/intro/TechCommunication.svg',
-    description: (
-      <>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam a nunc quis ligula venenatis maximus ac hendrerit urna. In diam enim, facilisis vitae mattis et, mattis non magna. Nullam condimentum in dui nec molestie. Nunc faucibus, erat sit amet tincidunt egestas, est enim rutrum massa, non sagittis est tortor a diam. Duis rhoncus interdum dolor eget posuere. Vestibulum aliquam vitae augue commodo pretium. Curabitur vel eros velit. Cras nisl ex, commodo eget massa vel, iaculis sodales nunc.
-      </>
-    )
-  },
-  {
-    title: <>Accelerating Your Open Source Delivery Pipeline</>,
-    imageUrl: '/img/intro/Success.svg',
-    description: (
-      <>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam a nunc quis ligula venenatis maximus ac hendrerit urna. In diam enim, facilisis vitae mattis et, mattis non magna. Nullam condimentum in dui nec molestie. Nunc faucibus, erat sit amet tincidunt egestas, est enim rutrum massa, non sagittis est tortor a diam. Duis rhoncus interdum dolor eget posuere. Vestibulum aliquam vitae augue commodo pretium. Curabitur vel eros velit. Cras nisl ex, commodo eget massa vel, iaculis sodales nunc.
-      </>
-    )
-  }
-];
 
 function Home() {
   const context = useDocusaurusContext();
