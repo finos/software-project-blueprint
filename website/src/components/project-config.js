2d8f4021fd73b9246eaccdc81fe0322d7b36fd7d
James McLeod <james.mcleod@finos.org>
2021-04-30 13:03:41 +0100

    add projects config

---
+++ website/src/components/project-config.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export const projects = [
+    {
+        "name": "FDC3",
+        "imageUrl": "https://raw.githubusercontent.com/finos/finos-landscape/master/hosted_logos/fdc3.svg",
+        "url": "https://fdc3.org"
+    },
+    {
+        "name": "Legend",
+        "imageUrl": "https://raw.githubusercontent.com/finos/finos-landscape/master/hosted_logos/legend.svg",
+        "url": "https://legend.finos.org"
+    },
+    {
+        "name": "Plexus Interop",
+        "imageUrl": "https://raw.githubusercontent.com/finos/finos-landscape/master/hosted_logos/plexus-interop.svg",
+        "url": "https://finos-plexus.github.io/plexus-interop"
+    }
+];
--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -11,24 +11,7 @@ import Feature from '../components/feature';
 import Vendor from '../components/vendor';
 
 import { features } from '../components/feature-config';
-
-const projects = [
-  {
-    "name": "FDC3",
-    "imageUrl": "https://raw.githubusercontent.com/finos/finos-landscape/master/hosted_logos/fdc3.svg",
-    "url": "https://fdc3.org"
-  },
-  {
-    "name": "Legend",
-    "imageUrl": "https://raw.githubusercontent.com/finos/finos-landscape/master/hosted_logos/legend.svg",
-    "url": "https://legend.finos.org"
-  },
-  {
-    "name": "Plexus Interop",
-    "imageUrl": "https://raw.githubusercontent.com/finos/finos-landscape/master/hosted_logos/plexus-interop.svg",
-    "url": "https://finos-plexus.github.io/plexus-interop"
-  }
-]
+import { projects } from '../components/project-config';
 
 const vendors = [
   {
