9a6708c51e3b56d52142d965c1b267ce12e24bfc
Juan Estrella <juan.estrella@finos.org>
2021-10-25 18:56:29 +0200

    Delete website/.docusaurus directory

--- website/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/juani/Desktop/Software Project Blueprint/My Fork/software-project-blueprint/website/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/juani/Desktop/Software Project Blueprint/My Fork/software-project-blueprint/website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/juani/Desktop/Software Project Blueprint/My Fork/software-project-blueprint/website/node_modules/@docusaurus/theme-classic/lib/admonitions.css"),
-  require("/Users/juani/Desktop/Software Project Blueprint/My Fork/software-project-blueprint/website/src/css/custom.css"),
-];
